a82f3062b60948645d900c6a05dfea85839f84c5
Brendan Barnes <brendanbarnessoccer@gmail.com>
2026-08-19 17:30:23 -0400

    Setup

---
+++ Hotel.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+/* CLASSES + METHODS
+    occupants - stay duration
+    rooms - room #, rate, floor #, isOccupied
+    floors - number
+    player - dollars, name, age
+
+*/
+
+int main(){
+    /* GAME LOOP
+        Welcome player, set up some basic stuff, name, hotel stuff, etc
+        
+        create new occupant, ask player where to put in hotel -> draw on screen
+        put occupant in room, room now occupied -> draw updated on screen
+
+        end of day calculate profit and add to player balance
+        
+        ask if they want to upgrade hotel -> buy more floors/rooms
+
+        proceed to next day
+
+    */
+    
+    std::cout << "Hello, there!";
+
+    return 0;
+}
