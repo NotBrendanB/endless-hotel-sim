336c963d50643083405140fcb4ea669cb21e7ab0
Brendan Barnes <brendanbarnessoccer@gmail.com>
2026-08-20 17:12:32 -0400

    Finish Classes, Began Main Setup

--- Hotel.cpp
+++ Hotel.cpp
@@ -12,17 +12,103 @@ class Occupant {
     const std::string& getName() const {
         return name;
     }
-    void setName(const std::string& inputName) {
-        name = inputName;
+    void setName(const std::string& newName) {
+        name = newName;
     }
     int getStayDuration() const {
         return stayDuration;
     }
-    void setStayDuration(int duration) {
-        stayDuration = duration;
+    void setStayDuration(int newDuration) {
+        stayDuration = newDuration;
     }
 };
-
+class Player {
+    private:
+    int money;
+    std::string name;
+    int age;
+    public:
+    Player(int p_money, const std::string& p_name, int p_age) 
+    : money(p_money),
+    name(p_name),
+    age(p_age)
+    {
+    }
+    int getMoney() const {
+        return money;
+    }
+    void setMoney(int newMoney) {
+        money = newMoney;
+    }
+    const std::string& getName() const { 
+        return name;
+    }
+    void setName(const std::string& newName) {
+        name = newName;
+    }
+};
+class Floor {
+    private:
+    int numberFloors;
+    int numberRooms;
+    public:
+    Floor(int n_Floors, int n_Rooms)
+    : numberFloors(n_Floors),
+    numberRooms(n_Rooms)
+    {
+    }
+    int getNumberFloors() const{
+        return numberFloors;
+    }
+    void setNumberFloors(int newNumberFloors) {
+        numberFloors = newNumberFloors;
+    }
+    int getNumberRooms() const {
+        return numberRooms;
+    }
+    void setNumberRooms(int newNumberRooms) {
+        numberRooms = newNumberRooms;
+    }
+};
+class Room {
+    private:
+    int roomNumber;
+    int rate;
+    int floorNumber;
+    bool isOccupied;
+    public:
+    Room(int r_RoomNumber, int r_rate, int r_FloorNumber, bool r_IsOccupied)
+    : roomNumber(r_RoomNumber),
+    rate(r_rate),
+    floorNumber(r_FloorNumber),
+    isOccupied(r_IsOccupied)
+    {
+    }
+    int getRoomNumber() const {
+        return roomNumber;
+    }
+    void setRoomNumber(int newRoomNumber) {
+        roomNumber = newRoomNumber;
+    }
+    int getRate() const {
+        return rate;
+    }
+    void setRate(int newRate) {
+        rate = newRate;
+    }
+    int getFloorNumber() const {
+        return floorNumber;
+    }
+    void setFloorNumber(int newFloorNumber) {
+        floorNumber = newFloorNumber;
+    }
+    bool getIsOccupied() const {
+        return isOccupied;
+    }
+    void setIsOccupied(bool newIsOccupied) {
+        isOccupied = newIsOccupied;
+    }
+};
 /* CLASSES + METHODS
     occupants - stay duration, name
     rooms - room #, rate, floor #, isOccupied
@@ -45,8 +131,22 @@ int main(){
         proceed to next day
 
     */
+    std::string nameInput;
+    std::string hotelNameInput;
+
+    std::cout << "**********************************\n";
+    std::cout << "    Welcome to Hotel Simulator   \n";
+    std::cout << "**********************************\n\n";
+
+    std::cout << "What is your name?: ";
+    std::cin >> nameInput;
+    std::cout << "Hello " << nameInput << "!\n\n";
+
+    std::cout <<"What would you like to name your Hotel?: ";
+    std::cin >> hotelNameInput;
+
     
-    std::cout << "Hello, there!";
+
 
     return 0;
 }
